3bdcabfeeb57ad59e3ae84d913314d9f32fd8102
Nicholas Draper <ndraper2@gmail.com>
2015-02-09 22:25:40 -0800

    changed server to allow more than one connection before shutting down, implemented unicode character test

--- echo_server.py
+++ echo_server.py
@@ -5,10 +5,13 @@ server_socket = socket.socket(
     socket.SOCK_STREAM,
     socket.IPPROTO_IP)
 server_socket.bind(('127.0.0.1', 50000))
-server_socket.listen(1)
-conn, addr = server_socket.accept()
-msg = conn.recv(4096)
-out = "{}{}".format("I heard: ", msg)
-conn.sendall(out)
-conn.close()
+server_socket.listen(10)
+i = 2  # the exact number of tests we have
+while i > 0:
+    conn, addr = server_socket.accept()
+    msg = conn.recv(4096)
+    out = "{}{}".format("I heard: ", msg)
+    conn.sendall(out)
+    conn.close()
+    i -= 1
 server_socket.close()
--- test_echo.py
+++ test_echo.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
 import os
 
 
 def test_simple_string(capfd):
     os.system("python echo_client.py 'This is the message to send'")
     out, err = capfd.readouterr()
-    print out
-    print err
     assert out == "I heard: This is the message to send\n"
+
+
+def test_unicode_string(capfd):
+    msg = u"python echo_client.py 'test character ó'".encode('utf-8')
+    os.system(msg)
+    out, err = capfd.readouterr()
+    assert out == u"I heard: test character ó\n"
