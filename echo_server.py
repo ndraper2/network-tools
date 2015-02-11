5d96bd0862b2ed34ba01e66ceb565efd4ee96acf
Nicholas Draper <ndraper2@gmail.com>
2015-02-11 08:37:16 -0800

    changed server to listen until a keyboard interrupt

--- echo_server.py
+++ echo_server.py
@@ -1,25 +1,27 @@
 import socket
 
-server_socket = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_IP)
-server_socket.bind(('127.0.0.1', 50000))
-server_socket.listen(10)
-i = 4  # the exact number of tests we have
-while i > 0:
-    conn, addr = server_socket.accept()
-    buffsize = 16
-    msg = ""
-    done = False
-    while not done:
-        part = conn.recv(buffsize)
-        print part
-        if len(part) < buffsize:
-            done = True
-        msg = "{}{}".format(msg, part)
-    out = "{}{}".format("I heard: ", msg)
-    conn.sendall(out)
-    conn.close()
-    i -= 1
-server_socket.close()
+
+def server_listener():
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    server_socket.bind(('127.0.0.1', 50000))
+    server_socket.listen(10)
+    try:
+        while True:
+            conn, addr = server_socket.accept()
+            buffsize = 16
+            msg = ""
+            done = False
+            while not done:
+                part = conn.recv(buffsize)
+                print part
+                if len(part) < buffsize:
+                    done = True
+                msg = "{}{}".format(msg, part)
+            out = "{}{}".format("I heard: ", msg)
+            conn.sendall(out)
+            conn.close()
+    except KeyboardInterrupt:
+        server_socket.close()
